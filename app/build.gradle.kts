df64a2ed7c93af11d4cea78f1d0936d69a7109a2
henryrts <115428130+henryrts@users.noreply.github.com>
2026-07-23 02:40:24 +0200

    Bump app version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.scrolldock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
