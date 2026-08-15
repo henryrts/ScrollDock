0e3f353f627dc00493599040cf7d2ea5ec726f74
henryrts <115428130+henryrts@users.noreply.github.com>
2026-08-15 23:36:11 +0200

    Remove quick prompt tests

--- app/src/test/java/com/scrolldock/FirstReleaseFeatureTest.kt
+++ app/src/test/java/com/scrolldock/FirstReleaseFeatureTest.kt
@@ -6,45 +6,6 @@ import org.junit.Test
 
 class FirstReleaseFeatureTest {
     @Test
-    fun insertsPhraseAtCursorWithoutDeletingExistingText() {
-        val result = QuickPhraseText.insert(
-            current = "Explain please",
-            phrase = "this ",
-            rawStart = 8,
-            rawEnd = 8,
-        )
-
-        assertEquals("Explain this please", result.text)
-        assertEquals(13, result.cursor)
-    }
-
-    @Test
-    fun replacesOnlySelectedText() {
-        val result = QuickPhraseText.insert(
-            current = "Explain old answer",
-            phrase = "new",
-            rawStart = 8,
-            rawEnd = 11,
-        )
-
-        assertEquals("Explain new answer", result.text)
-        assertEquals(11, result.cursor)
-    }
-
-    @Test
-    fun invalidSelectionFallsBackToEnd() {
-        val result = QuickPhraseText.insert(
-            current = "Continue",
-            phrase = " please",
-            rawStart = -1,
-            rawEnd = -1,
-        )
-
-        assertEquals("Continue please", result.text)
-        assertEquals(15, result.cursor)
-    }
-
-    @Test
     fun mapsOnlyScrollCommandsToDirections() {
         assertEquals(ScrollDirection.UP, ScrollCommand.PAGE_UP.scrollDirectionOrNull())
         assertEquals(ScrollDirection.UP, ScrollCommand.TOP.scrollDirectionOrNull())
